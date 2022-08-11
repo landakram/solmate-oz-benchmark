1371868275bba0a1482cdcdfe2beece1198fced5
Mark Hudnall <me@markhudnall.com>
2022-08-11 16:01:28 -0700

    Add benchmark

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,47 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+import {Authority} from "solmate/auth/Auth.sol";
+import "solmate/auth/authorities/RolesAuthority.sol";
+import "openzeppelin-contracts/contracts/access/AccessControl.sol";
+
+contract OZAccessControl is AccessControl {
+  bytes32 public constant TEST_ROLE = keccak256("TEST_ROLE");
+
+  event Foo();
+
+  constructor(address _owner) {
+    _setupRole(DEFAULT_ADMIN_ROLE, _owner);
+  }
+
+  function setupRole() public {
+    // Should never allow this to be publicly callable in production
+    _setupRole(TEST_ROLE, msg.sender);
+  }
+
+
+  function accessControlledMethod() public {
+    require(hasRole(TEST_ROLE, msg.sender), "Access denied");
+    emit Foo();
+  }
+}
+
+contract SolmateAccessControl is RolesAuthority {
+  uint8 public constant TEST_ROLE = 1;
+
+  event Foo();
+
+  constructor(address _owner) RolesAuthority(_owner, Authority(address(this))) {
+    
+  }
+
+  function setupRole() public {
+    // Should never allow this to be publicly callable in production
+    setUserRole(msg.sender, TEST_ROLE, true);
+    setRoleCapability(TEST_ROLE, address(this), bytes4(keccak256("accessControlledMethod()")), true);
+  }
+
+  function accessControlledMethod() public requiresAuth {
+    emit Foo();
+  }
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,12 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "forge-std/console.sol";
 import "forge-std/Test.sol";
+
+import {OZAccessControl, SolmateAccessControl} from "../src/Contract.sol";
 
 contract ContractTest is Test {
     function setUp() public {}
 
-    function testExample() public {
-        assertTrue(true);
+    function testOz() public {
+        OZAccessControl ozAccessControl = new OZAccessControl(address(this));
+        ozAccessControl.setupRole();
+        ozAccessControl.accessControlledMethod();
+    }
+
+    function testSolmate() public {
+        SolmateAccessControl solmateAccessControl = new SolmateAccessControl(address(this));
+        solmateAccessControl.setupRole();
+        solmateAccessControl.accessControlledMethod();
     }
 }
